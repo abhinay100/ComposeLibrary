48a3e25943f840c0700ae166795de167b833d984
abhinay <abhinay212@gmail.com>
2025-02-06 14:08:06 +0530

    Initial commit

---
+++ app/src/main/java/com/abhinay/androidlibrary/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.abhinay.androidlibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abhinay.androidlibrary.ui.theme.AndroidLibraryTheme
+import com.abhinay.image_preview.ImagePreview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidLibraryTheme {
+
+                ImagePreview(
+
+                )
+
+            }
+        }
+    }
+}
+

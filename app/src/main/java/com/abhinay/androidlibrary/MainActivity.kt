64ea7b8ef4f79870729561a3a8b174775c3198de
abhinay <abhinay212@gmail.com>
2025-02-07 13:48:04 +0530

    Update app to use ComposeLibrary 1.5
    - Updated `app/build.gradle` to replace `image-preview` project dependency with `com.github.abhinay100:ComposeLibrary:1.5`.
    - Removed unused `ImagePreview` component import in `MainActivity.kt`.
    - Commented out the `ImagePreview` component usage in `MainActivity.kt` and keep only an empty content.

--- app/src/main/java/com/abhinay/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/abhinay/androidlibrary/MainActivity.kt
@@ -21,6 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidLibraryTheme {
 
+                /*ImagePreview(
+
+                )
+*/
 
             }
         }
